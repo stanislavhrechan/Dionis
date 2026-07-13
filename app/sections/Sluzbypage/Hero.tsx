393c1c9503c033a2cf2ea6ecc73bafbae9395b1a
StanislavHrechan <network.stanislav@gmail.com>
2026-07-13 11:55:48 +0200

    Update hero with setction services

--- app/sections/Sluzbypage/Hero.tsx
+++ app/sections/Sluzbypage/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
         <section className="relative mt-10 flex flex-col justify-center">
             <span className="font-dm-mono font-light text-center mb-4 text-black/85 text-sm">VYBERTE SI SLUŽBU</span>
             <h1 className="font-instrument text-4xl md:text-6xl uppercase text-center mx-auto max-w-4xl">
-                Zaujímajú nás len výsledky našej práce a spokojnosť klientov.
+                Zaujímajú nás výsledky našej práce a spokojnosť klientov.
             </h1>
         </section>
         </>
